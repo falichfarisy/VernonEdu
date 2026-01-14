1c95fef04a44f0be5d44f902bb1c78d1d8b52816
Maudatul Mufidah <mauddaff@gmail.com>
2026-01-14 13:09:25 +0700

    menambahkan pallete warna

---
+++ lib/constants/colors.dart
@@ -0,0 +1,15 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+class CustomColor {
+  static const Color purple = Color (0xff955EAB);
+  static const Color blue = Color (0xff6AD6F8);
+  static const Color black = Color (0xffffffff);
+  static const Color white = Color (0xff000000);        
+  static const Color green = Color (0xff8FBE55);
+  static const Color yellow = Color (0xffF6D14B);
+  static const Color warning = Color (0xffFF0004);
+  static const Color abuMuda = Color (0xffBABABA);
+  static const Color abuTua = Color (0xff515151);
+  static const Color abuLogin = Color (0xffF6F7FB);
+}
