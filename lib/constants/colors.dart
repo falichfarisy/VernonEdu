60b7ee04d5bc43a3c0221cad138beb8256ea757d
Maudatul Mufidah <mauddaff@gmail.com>
2026-01-14 14:50:43 +0700

    update file colors

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -4,8 +4,8 @@ import 'package:flutter/material.dart';
 class CustomColor {
   static const Color purple = Color (0xff955EAB);
   static const Color blue = Color (0xff6AD6F8);
-  static const Color black = Color (0xffffffff);
-  static const Color white = Color (0xff000000);        
+  static const Color white = Color (0xffffffff);
+  static const Color black = Color (0xff000000);        
   static const Color green = Color (0xff8FBE55);
   static const Color yellow = Color (0xffF6D14B);
   static const Color warning = Color (0xffFF0004);
